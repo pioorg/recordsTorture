c1c26a09178a1f13afb84bba2fb71f2b1cdf9489
Piotr <piotr@przybyl.org>
2020-05-28 23:02:53 +0200

    Added records with Lombok.
    Don't do this at home.

---
+++ src/main/java/dev/softwaregarden/records/torture/lombok/LombokAndRecordsCheck.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (C) 2020 Piotr Przybył
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.softwaregarden.records.torture.lombok;
+
+public class LombokAndRecordsCheck {
+
+	public static void main(String[] args) {
+		System.out.println(new PlainRecord(1, "2"));
+		System.out.println(new RecordWithGetters(1, "2"));
+		System.out.println(new RecordWithAllArgsConstructor(1, "2"));
+		System.out.println(new RecordWithValue(1, "2"));
+	}
+
+}
